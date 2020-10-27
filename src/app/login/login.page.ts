6852ef2a467370c43a90fc8b849431e5da7e423b
Cyber-siqueira <felipe.siqueira@alunos.sc.senac.br>
2020-10-27 14:39:21 -0300

    commit
    comitando

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { errorMonitor } from 'events';
 import { AuthService } from '../shared/auth.service';
 
 @Component({
@@ -19,7 +20,17 @@ export class LoginPage implements OnInit {
         debugger;
       })
       .catch((error) => {
-        debugger;
+        let msg = "";
+        switch (error.code) {
+          case "auth/invalid-email":
+            msg = "O endereço de e-mail está em um formato incorreto.";
+          case "auth/user-not-found":
+            msg = "O e-mail digitado não está associado a nenhum usuário";
+          case "auth/wrong-password":
+            msg = "Senha Incorreta";
+
+        }
+        window.alert(msg);
       })
   }
 
